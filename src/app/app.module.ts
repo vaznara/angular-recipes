c6c7de6debeb02bee66775e0927d4795e05be670
v.titov <v.titov@faberlic.com>
2023-05-18 19:07:00 +0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { recipeBook } from '../recipe-book.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: recipeBook[] = [
+    new recipeBook(
+      'A Test Recipe',
+      'test Recipe description',
+      'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJlY2lwZXxlbnwwfHwwfHw%3D&w=1000&q=80'
+    ),
+    new recipeBook(
+      'A Test Recipe 2 ',
+      'test Recipe description 2',
+      'https://i.ndtvimg.com/i/2015-10/urlai-roast_625x350_71444723419.jpg'
+    ),
+    new recipeBook(
+      'A Test Recipe 3 ',
+      'test Recipe description 3',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9iMSUmvHYCX4iAHf9bXQmRLm0jZMC-89RKA&usqp=CAU'
+    ),
+  ];
 
+  constructor() {
+
+  }
+
+  createNewRecipe() {
+    
+  }
 }
